d7be1078b762cc5672a336c25d4869fb15316fb1
Prayudaa <prayuda040305@gmail.com>
2026-08-08 23:51:47 +0700

    chore: optimize SEO and performance

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = "62895350846090";
+export const WHATSAPP_NUMBER = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || "62895350846090";
 export const WHATSAPP_MESSAGE = encodeURIComponent(
   "Halo Kak Yuda, saya tertarik diskusi soal pembuatan website."
 );
